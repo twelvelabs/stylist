6f355bf055fa000ec891d17a3e852f4aa1d3ed2a
Skip Baney <twelvelabs@gmail.com>
2023-02-21 14:04:37 -0600

    feat: implement regexp parser
    Closes #2

--- internal/stylist/output_parser_test.go
+++ internal/stylist/output_parser_test.go
@@ -1,7 +1,10 @@
 package stylist
 
 import (
+	"bytes"
+	"io"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,7 +26,7 @@ func TestNewOutputParser(t *testing.T) {
 }
 
 func TestJSONOutputParser_Parse(t *testing.T) {
-	file, err := os.Open("testdata/json/shellcheck.json")
+	file, err := os.Open("testdata/output/shellcheck.json")
 	assert.NoError(t, err)
 
 	parser := &JSONOutputParser{}
@@ -63,4 +66,134 @@ func TestJSONOutputParser_Parse(t *testing.T) {
 			URI:         "https://www.shellcheck.net/wiki/SC2086",
 		},
 	}, results[0])
+}
+
+func TestRegexpOutputParser_Parse(t *testing.T) {
+	fixtureFile := func(path string) io.Reader {
+		f, err := os.Open(path)
+		if err != nil {
+			panic(err)
+		}
+		return f
+	}
+
+	mapping := ResultMapping{
+		Pattern: strings.Join([]string{
+			`(?m)`,
+			`Secret:\s+(?P<secret>.*)`,
+			`RuleID:\s+(?P<rule_id>.*)`,
+			`Entropy:\s+(?P<entropy>.*)`,
+			`File:\s+(?P<file>.*)`,
+			`Line:\s+(?P<line>.*)`,
+		}, `\n`),
+		Level:           render.MustCompile(`error`),
+		Path:            render.MustCompile(`{{ .file }}`),
+		StartLine:       render.MustCompile(`{{ .line }}`),
+		RuleID:          render.MustCompile(`{{ .rule_id }}`),
+		RuleName:        render.MustCompile(`{{ .rule_id }}`),
+		RuleDescription: render.MustCompile(`Secret detected: {{ .secret }}`),
+	}
+
+	tests := []struct {
+		desc     string
+		content  io.Reader
+		mapping  ResultMapping
+		expected []*Result
+		err      string
+	}{
+		{
+			desc:    "returns an error if pattern is missing",
+			content: fixtureFile("testdata/output/gitleaks.txt"),
+			mapping: ResultMapping{
+				Pattern: "",
+			},
+			expected: nil,
+			err:      "pattern is required",
+		},
+		{
+			desc:    "returns an error if pattern is malformed",
+			content: fixtureFile("testdata/output/gitleaks.txt"),
+			mapping: ResultMapping{
+				Pattern: "(?lol)",
+			},
+			expected: nil,
+			err:      "error parsing regexp",
+		},
+		{
+			desc:     "returns an empty slice when no content",
+			content:  bytes.NewBufferString(""),
+			mapping:  mapping,
+			expected: nil,
+			err:      "",
+		},
+		{
+			desc:     "returns an empty slice when nothing matches",
+			content:  bytes.NewBufferString("welp"),
+			mapping:  mapping,
+			expected: nil,
+			err:      "",
+		},
+		{
+			desc:    "parses text using a regexp pattern",
+			content: fixtureFile("testdata/output/gitleaks.txt"),
+			mapping: mapping,
+			expected: []*Result{
+				{
+					Level: ResultLevelError,
+					Location: ResultLocation{
+						Path:      ".env",
+						StartLine: 8,
+					},
+					Rule: ResultRule{
+						ID:          "github-oauth",
+						Name:        "github-oauth",
+						Description: "Secret detected: REDACTED",
+					},
+				},
+				{
+					Level: ResultLevelError,
+					Location: ResultLocation{
+						Path:      "bin/run-in-container.sh",
+						StartLine: 22,
+					},
+					Rule: ResultRule{
+						ID:          "github-oauth",
+						Name:        "github-oauth",
+						Description: "Secret detected: REDACTED",
+					},
+				},
+				{
+					Level: ResultLevelError,
+					Location: ResultLocation{
+						Path:      "etc/githooks/commit-msg",
+						StartLine: 10,
+					},
+					Rule: ResultRule{
+						ID:          "github-oauth",
+						Name:        "github-oauth",
+						Description: "Secret detected: REDACTED",
+					},
+				},
+			},
+			err: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			actual, err := (&RegexpOutputParser{}).Parse(
+				CommandOutput{
+					Content: tt.content,
+				},
+				tt.mapping,
+			)
+
+			if tt.err == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.ErrorContains(t, err, tt.err)
+			}
+
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
 }
