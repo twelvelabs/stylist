fd7340fc6bad35c874663d7d8fb70dd5b3b30ba9
Skip Baney <twelvelabs@gmail.com>
2023-11-23 21:14:37 -0600

    feat: add checkstyle result format

--- internal/stylist/result_printer_test.go
+++ internal/stylist/result_printer_test.go
@@ -1,9 +1,11 @@
 package stylist
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/twelvelabs/termite/ui"
 )
 
@@ -21,6 +23,105 @@ func TestNewResultPrinter(t *testing.T) {
 		config.Output.Format = ResultFormat("unknown")
 		_ = NewResultPrinter(ios, config)
 	})
+}
+
+func TestCheckstylePrinter_Print(t *testing.T) {
+	results := []*Result{
+		{
+			Source: "test-linter",
+			Level:  ResultLevelError,
+			Location: ResultLocation{
+				Path:        "some/path/foo.go",
+				StartLine:   1,
+				StartColumn: 0,
+			},
+			Rule: ResultRule{
+				ID:          "rule-id1",
+				Name:        "rule-name1",
+				Description: "no start column",
+			},
+		},
+		{
+			Source: "test-linter",
+			Level:  ResultLevelWarning,
+			Location: ResultLocation{
+				Path:        "some/path/foo.go",
+				StartLine:   2,
+				StartColumn: 3,
+			},
+			Rule: ResultRule{
+				ID:          "rule-id2",
+				Name:        "rule-name2",
+				Description: "valid start column",
+			},
+		},
+		{
+			Source: "test-linter",
+			Level:  ResultLevelWarning,
+			Location: ResultLocation{
+				Path:        "some/path/bar.go",
+				StartLine:   4,
+				StartColumn: 5,
+			},
+			Rule: ResultRule{
+				ID:          "rule-id2",
+				Name:        "rule-name2",
+				Description: "another valid start column",
+			},
+		},
+	}
+
+	tests := []struct {
+		desc     string
+		results  []*Result
+		expected string
+		err      string
+	}{
+		{
+			desc:     "empty result set should print nothing",
+			results:  []*Result{},
+			expected: `<?xml version="1.0" encoding="UTF-8"?><checkstyle version="4.3"></checkstyle>`,
+		},
+
+		{
+			desc:     "should print a minimal result set when config disabled",
+			results:  results,
+			expected: `<?xml version="1.0" encoding="UTF-8"?><checkstyle version="4.3"><file name="some/path/foo.go"><error line="1" column="0" message="no start column [rule-id1]" severity="error" source="test-linter"></error><error line="2" column="3" message="valid start column [rule-id2]" severity="warning" source="test-linter"></error></file><file name="some/path/bar.go"><error line="4" column="5" message="another valid start column [rule-id2]" severity="warning" source="test-linter"></error></file></checkstyle>`, //nolint: lll
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			app := NewTestApp()
+			printer := &CheckstylePrinter{
+				ios:    app.IO,
+				config: app.Config,
+			}
+			err := printer.Print(tt.results)
+
+			if tt.err == "" {
+				require.NoError(t, err)
+			} else {
+				require.ErrorContains(t, err, tt.err)
+			}
+
+			out := app.IO.Out.String()
+			out = strings.ReplaceAll(out, "\n", "")
+			assert.Equal(t, tt.expected, out)
+		})
+	}
+}
+
+func TestSarifPrinter_Print(t *testing.T) {
+	app := NewTestApp()
+	printer := &SarifPrinter{
+		ios:    app.IO,
+		config: app.Config,
+	}
+	results := []*Result{}
+
+	err := printer.Print(results)
+	assert.NoError(t, err)
 }
 
 func TestTtyPrinter_Print(t *testing.T) {
